7c51d252e4ebc2c5b179c903161d36f3a9c6fcc8
Michael Shaughnessy <mickeyshaughnessy@gmail.com>
2015-05-14 11:27:55 -0700

    add setup script

---
+++ setup_script.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt-get install git
+sudo apt-get install lamp-server^
+sudo apt-get install libapache2-mod-wsgi
+sudo service apache2 restart
+sudo apt-get install python-setuptools
+sudo apt-get install python-pip
+sudo pip install flask
+sudo pip install virtualenv
+sudo apt-get install lynx-cur
+
+cd /var/wwww/
+sudo mkdir PGTD
+cd PGTD
+sudo mkdir PGTD
+cd PGTD
+sudo mkdir static templates
